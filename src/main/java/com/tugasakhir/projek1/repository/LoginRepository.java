c1b7638ac06ab6251a178b80b1319a7b32cbbb56
elisaseptiani1009 <elisaseptianlubis1009@gmail.com>
2021-11-30 08:19:25 +0700

    end

--- src/main/java/com/tugasakhir/projek1/repository/LoginRepository.java
+++ src/main/java/com/tugasakhir/projek1/repository/LoginRepository.java
@@ -4,13 +4,16 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
 import com.tugasakhir.projek1.model.Login;
 
-public interface LoginRepository extends JpaRepository<Login,Long> {
-	
-	Optional<Login> findByUsername(String username);
-	
-	@Query("select a from Login a where a.akses = ?1")
-	Login cekRole(String username);
+public interface LoginRepository extends JpaRepository<Login, Long> {
+
+    Optional<Login> findByUsername(String username);
+
+    @Query("select a from Login a where a.akses = ?1")
+    Login cekRole(String username);
+
+    @Query(value = "select top 1 from login a where a.akses = ?1 ", nativeQuery = true)
+    Login cekRoleNative(String username);
+
 }
